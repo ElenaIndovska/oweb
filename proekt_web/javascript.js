f844fc525af558d72e4d00ddee31bde3461361fc
ElenaIndovska <indovski777@gmail.com>
2020-11-18 13:47:03 +0100

    update

---
+++ proekt_web/javascript.js
@@ -0,0 +1,79 @@
+/*function myFunction() {
+    document.getElementById("demo").innerHTML = "Hello World";
+
+}*/
+
+
+var counter1=0;
+var counter2=0;
+var counter3=0;
+var counter4=0;
+var counter5=0;
+var counter6=0;
+var counter7=0;
+var counter8=0;
+var counter9=0;
+var counter10=0;
+var counter11=0;
+var counter12=0;
+function like1(){
+    counter1++;
+    document.getElementById("result1").innerHTML=counter1;    
+}
+function like2(){
+    counter2++;
+    document.getElementById("result2").innerHTML=counter2;    
+}
+function like3(){
+    counter3++;
+    document.getElementById("result3").innerHTML=counter3;    
+}
+function like4(){
+    counter4++;
+    document.getElementById("result4").innerHTML=counter4;    
+}
+function like5(){
+    counter5++;
+    document.getElementById("result5").innerHTML=counter5;    
+}
+function like6(){
+    counter6++;
+    document.getElementById("result6").innerHTML=counter6;    
+}
+function like7(){
+    counter7++;
+    document.getElementById("result7").innerHTML=counter7;    
+}
+function like8(){
+    counter8++;
+    document.getElementById("result8").innerHTML=counter8;    
+}
+function like9(){
+    counter9++;
+    document.getElementById("result9").innerHTML=counter9;    
+}
+function like10(){
+    counter10++;
+    document.getElementById("result10").innerHTML=counter10;    
+}
+function like11(){
+    counter11++;
+    document.getElementById("result11").innerHTML=counter11;    
+}
+function like12(){
+    counter12++;
+    document.getElementById("result12").innerHTML=counter12;    
+}
+
+document.getElementById("like1").onclick=like1();
+document.getElementById("like2").onclick=like2();
+document.getElementById("like3").onclick=like3();
+document.getElementById("like4").onclick=like4();
+document.getElementById("like5").onclick=like5();
+document.getElementById("like6").onclick=like6();
+document.getElementById("like7").onclick=like7();
+document.getElementById("like8").onclick=like8();
+document.getElementById("like9").onclick=like9();
+document.getElementById("like10").onclick=like10();
+document.getElementById("like11").onclick=like11();
+document.getElementById("like12").onclick=like12();
